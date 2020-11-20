aa418dbb49c45813995a147c950814b0f7cd5d3f
Simon Inkin <simon.inkin@gmail.com>
2020-11-21 00:30:24 +0200

    Tests Passing (no session controller tests)

--- test/parkin/accounts_test.exs
+++ test/parkin/accounts_test.exs
@@ -6,9 +6,24 @@ defmodule Parkin.AccountsTest do
   describe "users" do
     alias Parkin.Accounts.User
 
-    @valid_attrs %{}
-    @update_attrs %{}
-    @invalid_attrs %{}
+    @valid_attrs %{
+      username: "amigo@grande.mx",
+      name: "Amigo Grande",
+      license_number: "BFG888",
+      password: "qwerty"
+    }
+    @update_attrs %{
+      username: "marko@x.com",
+      name: "Marc McLaughlin",
+      license_number: "XDOTCOM01",
+      password: "qwerty"
+    }
+    @invalid_attrs %{
+      username: nil,
+      name: nil,
+      license_number: nil,
+      password: nil
+    }
 
     def user_fixture(attrs \\ %{}) do
       {:ok, user} =
@@ -20,17 +35,17 @@ defmodule Parkin.AccountsTest do
     end
 
     test "list_users/0 returns all users" do
-      user = user_fixture()
+      user = user_fixture() |> Map.replace!(:password, nil)
       assert Accounts.list_users() == [user]
     end
 
     test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+      user = user_fixture() |> Map.replace!(:password, nil)
       assert Accounts.get_user!(user.id) == user
     end
 
     test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
+      assert {:ok, %User{} = _user} = Accounts.create_user(@valid_attrs)
     end
 
     test "create_user/1 with invalid data returns error changeset" do
@@ -39,11 +54,11 @@ defmodule Parkin.AccountsTest do
 
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
-      assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
+      assert {:ok, %User{} = _user} = Accounts.update_user(user, @update_attrs)
     end
 
     test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
+      user = user_fixture() |> Map.replace!(:password, nil)
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       assert user == Accounts.get_user!(user.id)
     end
--- test/parkin_web/controllers/user_controller_test.exs
+++ test/parkin_web/controllers/user_controller_test.exs
@@ -1,11 +1,48 @@
 defmodule ParkinWeb.UserControllerTest do
   use ParkinWeb.ConnCase
 
+  alias Parkin.Guardian
   alias Parkin.Accounts
 
-  @create_attrs %{}
-  @update_attrs %{}
-  @invalid_attrs %{}
+  @create_attrs %{
+    username: "amigo@grande.mx",
+    name: "Amigo Grande",
+    license_number: "BFG888",
+    password: "qwerty",
+    hashed_password:
+      "$pbkdf2-sha512$160000$RXDATuB9gUGqmhgt5NiHUg$W81M1SedlBMVY5wOwlBhNrr2eJ0O2EfRwvbi0L03O7qfVBxTWy6zlfmFIPr93bqZNaD5EQXIHjYSF19L9a/hwg"
+  }
+  @update_attrs %{
+    username: "marko@x.com",
+    name: "Marc McLaughlin",
+    license_number: "XDOTCOM01",
+    password: "qwerty"
+  }
+  @invalid_attrs %{
+    username: nil,
+    name: nil,
+    license_number: nil,
+    password: nil
+  }
+
+  def guardian_login(user) do
+    build_conn()
+    |> bypass_through(Parkin.Router, [:browser, :browser_authenticated_session])
+    |> get("/")
+    |> Map.update!(:state, fn _ -> :set end)
+    |> Guardian.Plug.sign_in(user)
+    |> send_resp(200, "Flush the session")
+    |> recycle
+  end
+
+  def guardian_logout(user) do
+    build_conn()
+    |> bypass_through(Parkin.Router, [:browser, :browser_authenticated_session])
+    |> get("/")
+    |> Guardian.Plug.sign_out(user)
+    |> send_resp(200, "Flush the session")
+    |> recycle
+  end
 
   def fixture(:user) do
     {:ok, user} = Accounts.create_user(@create_attrs)
@@ -13,6 +50,8 @@ defmodule ParkinWeb.UserControllerTest do
   end
 
   describe "index" do
+    setup [:create_user]
+
     test "lists all users", %{conn: conn} do
       conn = get(conn, Routes.user_path(conn, :index))
       assert html_response(conn, 200) =~ "Listing Users"
@@ -27,14 +66,9 @@ defmodule ParkinWeb.UserControllerTest do
   end
 
   describe "create user" do
-    test "redirects to show when data is valid", %{conn: conn} do
+    test "success flash msg when data is valid", %{conn: conn} do
       conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
-
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.user_path(conn, :show, id)
-
-      conn = get(conn, Routes.user_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show User"
+      assert get_flash(conn, :info) =~ "User created successfully."
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
@@ -75,6 +109,7 @@ defmodule ParkinWeb.UserControllerTest do
     test "deletes chosen user", %{conn: conn, user: user} do
       conn = delete(conn, Routes.user_path(conn, :delete, user))
       assert redirected_to(conn) == Routes.user_path(conn, :index)
+
       assert_error_sent 404, fn ->
         get(conn, Routes.user_path(conn, :show, user))
       end
@@ -83,6 +118,8 @@ defmodule ParkinWeb.UserControllerTest do
 
   defp create_user(_) do
     user = fixture(:user)
-    %{user: user}
+    conn = guardian_login(user)
+
+    %{user: user, conn: conn}
   end
 end
