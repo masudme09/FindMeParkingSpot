430a2c19fcaa2e006d9e77741ab482a572361509
Simon Inkin <simon.inkin@gmail.com>
2020-12-01 00:47:22 +0200

    Select Kastani works | Whitebread Context

--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -142,13 +142,14 @@ defmodule WhiteBreadContext do
     {:ok, state}
   end)
 
-  given_ ~r/^I am on the parking search page$/, fn state ->
+  given_(~r/^I am on the parking search page$/, fn state ->
     :timer.sleep(5000)
     navigate_to("/parking/search")
     {:ok, state}
-  end
+  end)
 
-  and_ ~r/^I have search for "(?<destination_address>[^"]+)" as destination location on the search page$/,
+  and_(
+    ~r/^I have search for "(?<destination_address>[^"]+)" as destination location on the search page$/,
     fn state, %{destination_address: destination_address} ->
       :timer.sleep(5000)
 
@@ -156,22 +157,25 @@ defmodule WhiteBreadContext do
       searchfld = find_within_element(form, :id, "searchtext")
       searchfld |> fill_field(destination_address)
 
-    :timer.sleep(5000)
+      :timer.sleep(5000)
 
-    {:ok, state}
-  end
+      {:ok, state}
+    end
+  )
 
-  when_ ~r/^I click on search button$/, fn state ->
-
+  when_(~r/^I click on search button$/, fn state ->
     click({:id, "submit-button"})
     {:ok, state}
-  end
+  end)
 
-  then_ ~r/^I should see available parking space summary on that location when parking slot is available.$/, fn state ->
-    :timer.sleep(5000)
-    assert visible_in_page?(~r/Available Parking spaces/)
-    {:ok, state}
-  end
+  then_(
+    ~r/^I should see available parking space summary on that location when parking slot is available.$/,
+    fn state ->
+      :timer.sleep(5000)
+      assert visible_in_page?(~r/Available Parking spaces/)
+      {:ok, state}
+    end
+  )
 
   # then_ ~r/^I should not see available parking space summary on that location when parking slot is not available.$/, fn state ->
   #   :timer.sleep(5000)
@@ -179,23 +183,35 @@ defmodule WhiteBreadContext do
   #   {:ok, state}
   # end
 
-  given_ ~r/^I am on the parking summary page$/, fn state ->
-    navigate_to("/parking/summary/Veski?latlong=58.37908%2C26.70908&dist=0.608")
-    {:ok, state}
-  end
+  given_(
+    ~r/^I am on the parking summary page for destination address "(?<destination_address>[^"]+)"$/,
+    fn state, %{destination_address: destination_address} ->
+      navigate_to("/parking/search")
+      form = find_element(:id, "search-form")
+      searchfld = find_within_element(form, :id, "searchtext")
+      searchfld |> fill_field(destination_address)
+      click({:id, "submit-button"})
+      :timer.sleep(20000)
+      assert visible_in_page?(~r/Available Parking spaces/)
 
-  when_ ~r/^I have selected parking location and I click on select button$/, fn state ->
+      {:ok, state}
+    end
+  )
+
+  when_(~r/^I have selected parking location and I click on select button$/, fn state ->
+    :timer.sleep(5000)
+
     find_element(:link_text, "Select Kastani")
     |> click()
+
     :timer.sleep(5000)
     {:ok, state}
-  end
+  end)
 
-  then_ ~r/^I should see parking space detail at that location.$/, fn state ->
+  then_(~r/^I should see parking space detail at that location.$/, fn state ->
     :timer.sleep(5000)
     assert visible_in_page?(~r/Parking summary/)
     :timer.sleep(5000)
     {:ok, state}
-  end
-
+  end)
 end
