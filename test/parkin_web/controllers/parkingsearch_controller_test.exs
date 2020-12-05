b1b236bfcf70467401468ceeecb3feb2125ee625
Masud Rana <marana@ttu.ee>
2020-12-05 16:32:56 +0200

    TDD test added from parking/search branch

--- test/parkin_web/controllers/parkingsearch_controller_test.exs
+++ test/parkin_web/controllers/parkingsearch_controller_test.exs
@@ -14,4 +14,36 @@ defmodule ParkinWeb.ParkingSearchControllerTest do
     assert html_response(conn, 200) =~ "No available Parking spaces"
   end
 
+  test "Only places inside the radius of 1km is displayed", %{conn: conn} do
+    conn = post(conn, Routes.parkingsearch_path(conn, :summary), searchtext: "Veski, Tartu, Estonia")
+    x = html_response(conn, 200)
+    b= Floki.find(x, "a")
+      |> Floki.attribute("href")
+    c = Enum.filter(b, fn x->
+      if String.contains?(x, "dist") do x end
+    end)
+
+    d= Enum.map(c, fn x ->
+      e = List.last( String.split(x,"&"))
+      f = List.last( String.split(x,"="))
+      {m , n} = Float.parse(f)
+      if m < 1.0 do m end
+    end)
+
+    assert length(d) > 0
+  end
+
+  #2.2 TDD
+  test "Availability for retrieved spots is always > 1", %{conn: conn} do
+    conn = post(conn, Routes.parkingsearch_path(conn, :summary), searchtext: "Veski, Tartu, Estonia")
+    x = html_response(conn, 200)
+    b= Floki.find(x, "a")
+      |> Floki.attribute("href")
+    c = Enum.filter(b, fn x->
+      if String.contains?(x, "dist") do x end
+    end)
+
+    assert length(b) > 0
+  end
+
 end
