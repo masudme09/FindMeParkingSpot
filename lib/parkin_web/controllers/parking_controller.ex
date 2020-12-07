f263e8395f96699dc4922962872f488c68569e95
Simon Inkin <simon.inkin@gmail.com>
2020-12-07 18:09:25 +0200

    confict postfix

--- lib/parkin_web/controllers/parking_controller.ex
+++ lib/parkin_web/controllers/parking_controller.ex
@@ -19,10 +19,6 @@ defmodule ParkinWeb.ParkingController do
     render(conn, "index.html", parkings: parkings)
   end
 
-  @spec new(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def new(conn, %{"loc_lat_long" => loc_lat_long}) do
-      slot = Parkin.Helper.Summary.get_zone_data_from(loc_lat_long)
-      
   def new(conn, params) do
     loc_lat_long = params["loc_lat_long"]
 
